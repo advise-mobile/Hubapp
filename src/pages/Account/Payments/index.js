2b9e80329ef45ccd01ce3d92935bb9073b93cbce
Advise Mobile <advisemobile@gmail.com>
2021-08-16 14:52:06 -0300

    Criação parcial da aba de central de pagamentos

---
+++ src/pages/Account/Payments/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Warp } from 'assets/styles/general';
+
+export default Infos = props => {
+  return (
+    <Container>
+      <Warp>
+
+      </Warp>
+    </Container>
+  );
+}
