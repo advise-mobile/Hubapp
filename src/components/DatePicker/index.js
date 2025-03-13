28b3f1fffe6b4ca47d9e50df18c159ac4a6021f0
Julio <juliohn@gmail.com>
2025-03-12 21:12:42 -0300

    Ajuste no datepicker
    Datepicker ajustado para portugues e aparecer conforme sistema operacional, no seu respectivo padrão, ios (rolagem com dia/me/ano ) para selecionar individualmente
    e no android aquele modal

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,27 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import {View, Platform, StyleSheet, TouchableOpacity, Text} from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import moment from 'moment';
-
-import DatePicker from 'react-native-datepicker';
-import {fonts} from 'assets/styles';
-
-// Add Hook UseTheme para pegar o tema global addicionado
+import 'moment/locale/pt-br';
 import {useTheme} from 'styled-components';
 
-const Datepicker = ({
-	date,
-	onDateChange,
-	title,
-	enabled,
-	style,
-	minDate,
-	maxDate,
-	customStyles,
-	error = false,
-}) => {
-	const [mode, setMode] = useState('date');
-	const [show, setShow] = useState(false);
+const Datepicker = ({date, onDateChange, title, enabled, minDate, maxDate}) => {
+	const [isDatePickerVisible, setDatePickerVisible] = useState(false);
 	const [selectedDate, setSelectedDate] = useState();
 
 	const convertDate = date => {
@@ -32,55 +17,52 @@ const Datepicker = ({
 		return date;
 	};
 
-	// Variavel para usar o hook
 	const colorUseTheme = useTheme();
 	const {colors} = colorUseTheme;
-
 	const styles = stylesDatePicker(colors);
 
 	useEffect(() => {
+		moment.locale('pt-br');
 		if (date) {
 			setSelectedDate(new Date(convertDate(date)));
 		}
 	}, [date]);
 
-	const showMode = currentMode => {
-		setShow(true);
-		setMode(currentMode);
+	const showDatePicker = () => {
+		setDatePickerVisible(true);
 	};
 
-	const onChange = (event, selectedDate) => {
-		const currentDate = selectedDate || date;
-		setShow(Platform.OS === 'ios');
-		setSelectedDate(currentDate);
-		onDateChange(currentDate);
+	const hideDatePicker = () => {
+		setDatePickerVisible(false);
 	};
 
-	const showDatepicker = () => {
-		showMode('date');
+	const handleConfirm = date => {
+		hideDatePicker();
+		setSelectedDate(date);
+		onDateChange(date);
 	};
 
 	return (
 		<View>
-			<View>
-				<TouchableOpacity onPress={showDatepicker} style={styles.datePicker}>
-					<Text style={styles.datePickerText}>
-						{selectedDate ? moment(selectedDate).format('DD/MM/YYYY') : title}
-					</Text>
-				</TouchableOpacity>
-			</View>
-			{enabled != false && show && (
-				<DateTimePicker
-					testID="dateTimePicker"
-					value={selectedDate || new Date()}
-					mode={mode}
-					is24Hour={true}
-					display="default"
-					onChange={onChange}
-					minimumDate={minDate}
-					maximumDate={maxDate}
-				/>
-			)}
+			<TouchableOpacity onPress={showDatePicker} disabled={!enabled} style={styles.datePicker}>
+				<Text style={styles.datePickerText}>
+					{selectedDate ? moment(selectedDate).format('DD/MM/YYYY') : title}
+				</Text>
+			</TouchableOpacity>
+
+			<DateTimePickerModal
+				isVisible={isDatePickerVisible}
+				mode="date"
+				onConfirm={handleConfirm}
+				onCancel={hideDatePicker}
+				date={selectedDate || new Date()}
+				minimumDate={minDate}
+				maximumDate={maxDate}
+				locale="pt-BR"
+				confirmTextIOS="Confirmar"
+				cancelTextIOS="Cancelar"
+				headerTextIOS="Selecione uma data"
+			/>
 		</View>
 	);
 };
@@ -90,14 +72,13 @@ var stylesDatePicker = () => {};
 if (Platform.OS === 'ios') {
 	stylesDatePicker = colors =>
 		StyleSheet.create({
-			input: {
-				// alignItems: 'flex-end',
-				height: 16,
-				justifyContent: 'center',
-				width: '100%',
-				fontSize: fonts.regular,
+			datePicker: {
+				marginBottom: 1,
+				borderColor: colors.grayLighter,
+				maxWidth: 150,
+			},
+			datePickerText: {
 				color: colors.primary,
-				fontFamily: fonts.circularStdBook,
 			},
 		});
 } else {
@@ -106,8 +87,7 @@ if (Platform.OS === 'ios') {
 			datePicker: {
 				marginBottom: 1,
 				borderColor: colors.grayLighter,
-				borderBottomWidth: 1,
-				maxWidth: 120,
+				maxWidth: 150,
 			},
 			datePickerText: {
 				color: colors.primary,
@@ -115,95 +95,4 @@ if (Platform.OS === 'ios') {
 		});
 }
 
-const DatepickerIOS = ({
-	date,
-	onDateChange,
-	title,
-	enabled,
-	style,
-	minDate,
-	maxDate,
-	customStyles,
-	error = false,
-}) => {
-	// Variavel para usar o hook
-	const colorUseTheme = useTheme();
-	const {colors} = colorUseTheme;
-
-	const styles = stylesDatePicker(colors);
-
-	return (
-		<DatePicker
-			style={{...styles.input, ...style}}
-			date={date || null}
-			mode="date"
-			onDateChange={onDateChange}
-			placeholder={title}
-			format="DD/MM/YYYY"
-			confirmBtnText="Confirmar"
-			cancelBtnText="Cancelar"
-			showIcon={false}
-			disabled={!enabled}
-			minDate={minDate}
-			maxDate={maxDate}
-			customStyles={
-				customStyles || {
-					btnTextConfirm: {
-						color: colors.white,
-					},
-					btnTextCancel: {
-						color: colors.primary,
-					},
-					btnConfirm: {
-						backgroundColor: colors.primary,
-						width: '49.5%',
-					},
-					btnCancel: {
-						backgroundColor: colors.white,
-						width: '49.5%',
-					},
-					dateInput: {
-						height: 20,
-						marginBottom: -3,
-						paddingBottom: 0,
-						borderLeftWidth: 0,
-						borderRightWidth: 0,
-						borderTopWidth: 0,
-						borderBottomWidth: 1,
-						borderBottomColor: enabled ? colors.grayLighter : colors.white,
-						flexDirection: 'row',
-						justifyContent: 'flex-start',
-						fontFamily: fonts.circularStdBook,
-						fontSize: fonts.regular,
-					},
-					placeholderText: {
-						color: error ? colors.red : colors.grayLight,
-						fontFamily: fonts.circularStdBook,
-						fontSize: fonts.regular,
-					},
-					disabled: {
-						backgroundColor: colors.white,
-						fontFamily: fonts.circularStdBook,
-						fontSize: fonts.regular,
-					},
-					dateText: {
-						color: colors.grayLight,
-						fontFamily: fonts.circularStdBook,
-						fontSize: fonts.regular,
-					},
-					datePicker: {
-						borderTopWidth: 0.2,
-						borderTopColor: colors.primary,
-						backgroundColor: colors.white,
-						fontFamily: fonts.circularStdBook,
-						fontSize: fonts.regular,
-					},
-				}
-			}
-		/>
-	);
-};
-
-const DatepickerComponent = Platform.OS === 'ios' ? DatepickerIOS : Datepicker;
-
-export default DatepickerComponent;
+export default Datepicker;
--- src/pages/Finance/Modal/ReleaseAdd/styles.ts
+++ src/pages/Finance/Modal/ReleaseAdd/styles.ts
@@ -1,66 +1,68 @@
 import styled from 'styled-components/native';
-import { fonts } from 'assets/styles';
+import {fonts} from 'assets/styles';
 
 interface IsErrorProps {
 	isError: boolean;
 }
 
 export const Footer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
 `;
 
 export const Cancel = styled.TouchableOpacity`
-  flex: 1;
-  padding: 7px;
-  margin-right: 12px;
-  background: ${({ theme }) => theme.colors.white};
-  justify-content: center;
-  align-items: center;
-  border-radius: 4px;
-  border-width: 1px;
-  border-color: ${({ theme }) => theme.colors.primary};
+	flex: 1;
+	padding: 7px;
+	margin-right: 12px;
+	background: ${({theme}) => theme.colors.white};
+	justify-content: center;
+	align-items: center;
+	border-radius: 4px;
+	border-width: 1px;
+	border-color: ${({theme}) => theme.colors.primary};
 `;
 
 export const Register = styled.TouchableOpacity`
-  flex: 1;
-  padding: 7px;
-  margin-right: 12px;
-  background: ${({ theme }) => theme.colors.primary};
-  justify-content: center;
-  align-items: center;
-  border-radius: 4px;
-  border-width: 1px;
-  border-color: ${({ theme }) => theme.colors.primary};
+	flex: 1;
+	padding: 7px;
+	margin-right: 12px;
+	background: ${({theme}) => theme.colors.primary};
+	justify-content: center;
+	align-items: center;
+	border-radius: 4px;
+	border-width: 1px;
+	border-color: ${({theme}) => theme.colors.primary};
 `;
 
 export const CancelText = styled.Text`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: ${fonts.small};
-  font-family: ${fonts.circularStdBold};
+	color: ${({theme}) => theme.colors.primary};
+	font-size: ${fonts.small};
+	font-family: ${fonts.circularStdBold};
 `;
 
 export const RegisterText = styled.Text`
-  color: ${({ theme }) => theme.colors.realWhite};
-  font-size: ${fonts.small};
-  font-family: ${fonts.circularStdBold};
+	color: ${({theme}) => theme.colors.realWhite};
+	font-size: ${fonts.small};
+	font-family: ${fonts.circularStdBold};
 `;
 
 export const ContentDescription = styled.View<IsErrorProps>`
-  	margin: 0 -24px;
+	margin: 0 -24px;
 	justify-content: center;
 	height: 60px;
 	width: 414px;
 	border-bottom-width: 0.3px;
-	border-bottom-color: ${({ isError, theme }) => (isError ? theme.colors.red200 : theme.colors.grayLighter)};
+	border-bottom-color: ${({isError, theme}) =>
+		isError ? theme.colors.red200 : theme.colors.grayLighter};
 `;
 
 export const Content = styled.View<IsErrorProps>`
-  margin: 0 -24px;
+	margin: 0 -24px;
 	justify-content: center;
 	border-bottom-width: 0.3px;
-	border-bottom-color: ${({ isError, theme }) => (isError ? theme.colors.red200 : theme.colors.grayLighter)};
+	border-bottom-color: ${({isError, theme}) =>
+		isError ? theme.colors.red200 : theme.colors.grayLighter};
 	height: 60px;
 	width: 414px;
 `;
@@ -72,7 +74,8 @@ export const Category = styled.View<IsErrorProps>`
 	flex-wrap: wrap;
 	align-items: center;
 	border-top-width: 0.3px;
-	border-top-color: ${({ isError, theme }) => (isError ? theme.colors.red200 : theme.colors.grayLighter)};
+	border-top-color: ${({isError, theme}) =>
+		isError ? theme.colors.red200 : theme.colors.grayLighter};
 `;
 
 export const ContainerCategories = styled.View`
@@ -91,45 +94,44 @@ export const ReleaseType = styled.TouchableOpacity`
 	margin-bottom: 7px;
 	align-items: center;
 	justify-content: center;
-	background-color: ${({ theme }) => theme.colors.gray};
+	background-color: ${({theme}) => theme.colors.gray};
 `;
-
-
 
 export const People = styled.View`
 	margin: 0 -24px;
 	border-top-width: 1px;
-  	border-top-color: ${({ theme }) => theme.colors.grayLighter};
+	border-top-color: ${({theme}) => theme.colors.grayLighter};
 	height: auto;
 	width: 414px;
 `;
 
 export const ContentRepeat = styled.View<IsErrorProps>`
-  /* margin: 0 -24px;
+	/* margin: 0 -24px;
   height: 116px;
   width: 414px;
   border-bottom-width: 1px;
   border-top-width: 1px;
-  border-color: ${({ isError, theme }) => (isError ? theme.colors.red200 : theme.colors.grayLighter)}; */
+  border-color: ${({isError, theme}) =>
+		isError ? theme.colors.red200 : theme.colors.grayLighter}; */
 
-  margin: 0 -24px;
+	margin: 0 -24px;
 	height: auto;
 	flex-direction: row;
 	flex-wrap: wrap;
 	align-items: center;
 	border-top-width: 0.3px;
-	border-top-color: ${({ theme }) => theme.colors.grayLighter};
+	border-top-color: ${({theme}) => theme.colors.grayLighter};
 `;
 
 export const Row = styled.View`
-  padding: 12px 24px;
-	width: 360px;
-	justify-content: space-between;
+	padding: 12px 24px;
+	flex: 1;
 	flex-direction: row;
+	align-items: center;
 `;
 
 export const RowLabel = styled.View`
-  	padding: 5px 24px;
+	padding: 5px 24px;
 	margin-top: 10px;
 	width: 360px;
 	flex-direction: row;
@@ -137,25 +139,26 @@ export const RowLabel = styled.View`
 `;
 
 export const Label = styled.Text`
-  margin-right: 12;
-  color: ${({ theme }) => theme.colors.BlackInactive};
-  font-size: ${fonts.regular};
-  font-family: ${fonts.circularStdBold};
+	margin-right: 12px;
+	min-width: 80px;
+	color: ${({theme}) => theme.colors.BlackInactive};
+	font-size: ${fonts.regular};
+	font-family: ${fonts.circularStdBold};
 `;
 
 export const LabelError = styled.Text`
-  color: ${({ theme }) => theme.colors.red200};
-  font-size: ${fonts.small};
-  font-family: ${fonts.circularStdBold};
+	color: ${({theme}) => theme.colors.red200};
+	font-size: ${fonts.small};
+	font-family: ${fonts.circularStdBold};
 `;
 
 export const Input = styled.TextInput`
-  flex: 1;
-  color: ${props => props.error ? props.theme.colors.red : props.theme.colors.grayLight};
-  font-size: ${fonts.regular};
-  font-family: ${fonts.circularStdBook};
-  height: 20px;
-  padding: 0;
+	flex: 1;
+	color: ${props => (props.error ? props.theme.colors.red : props.theme.colors.grayLight)};
+	font-size: ${fonts.regular};
+	font-family: ${fonts.circularStdBook};
+	height: 20px;
+	padding: 0;
 `;
 
 export const ContainerItems = styled.View`
@@ -168,9 +171,6 @@ export const ContainerItems = styled.View`
 	margin-bottom: 5px;
 	align-items: center;
 `;
-
-
-
 
 export const ContainerItemsOptions = styled.View`
 	width: 355px;
@@ -186,7 +186,7 @@ export const ContainerItemsOptions = styled.View`
 export const ItemsOptions = styled.TouchableOpacity`
 	border-radius: 10px;
 	max-width: 310px;
-	padding:6px;
+	padding: 6px;
 	height: 30px;
 	margin-right: 10px;
 	margin-bottom: 10px;
@@ -196,43 +196,41 @@ export const ItemsOptions = styled.TouchableOpacity`
 `;
 
 export const Process = styled.View`
-
 	margin: 0 -24px;
 	height: auto;
 	flex-direction: row;
 	flex-wrap: wrap;
 	align-items: center;
 	border-top-width: 0.3px;
-	border-top-color: ${({ theme }) => theme.colors.grayLighter};
+	border-top-color: ${({theme}) => theme.colors.grayLighter};
 `;
 
 export const LabelItemsProcess = styled.Text`
-  	font-size: ${fonts.smaller};
+	font-size: ${fonts.smaller};
 	font-family: ${fonts.circularStdBold};
 	margin-left: 4px;
 	margin-right: 4px;
 `;
 
 export const LabelItems = styled.Text`
-  font-size: ${fonts.smaller};
-  font-family: ${fonts.circularStdBold};
-  margin-right: 2px;
+	font-size: ${fonts.smaller};
+	font-family: ${fonts.circularStdBold};
+	margin-right: 2px;
 `;
 
-
 export const ContentDuring = styled.View<IsErrorProps>`
-  	margin: 0 -24px;
+	margin: 0 -24px;
 	justify-content: center;
 	height: 60px;
 	width: 414px;
-  	border-top-width: 0.3px;
-	border-color: ${({ isError, theme }) => (isError ? theme.colors.red200 : theme.colors.grayLighter)};
+	border-top-width: 0.3px;
+	border-color: ${({isError, theme}) => (isError ? theme.colors.red200 : theme.colors.grayLighter)};
 `;
 
 export const LabelDuring = styled.Text`
-  margin-right: 12;
-  color: ${({ theme }) => theme.colors.BlackInactive};
-  font-size: ${fonts.regular};
+	margin-right: 12;
+	color: ${({theme}) => theme.colors.BlackInactive};
+	font-size: ${fonts.regular};
 	font-family: ${fonts.circularStdBold};
 `;
 
@@ -243,32 +241,31 @@ export const ContainerInfo = styled.View`
 `;
 
 export const LabelDuringInfo = styled.Text`
-  margin-right: 12;
-  color: ${({ theme }) => theme.colors.darkGray};
-  font-size: ${fonts.regular};
+	margin-right: 12;
+	color: ${({theme}) => theme.colors.darkGray};
+	font-size: ${fonts.regular};
 `;
 
 export const ContentComments = styled.View<IsErrorProps>`
-  margin: 0 -24px;
+	margin: 0 -24px;
 	height: 110px;
 	width: 414px;
 	border-bottom-width: 0.3px;
 	border-top-width: 0.3px;
-	border-color: ${({ isError, theme }) => (isError ? theme.colors.red200 : theme.colors.grayLighter)};
+	border-color: ${({isError, theme}) => (isError ? theme.colors.red200 : theme.colors.grayLighter)};
 `;
 
 export const LabelComments = styled.Text`
-  margin-right: 12;
-  color: ${({ theme }) => theme.colors.BlackInactive};
-  font-size: ${fonts.regular};
+	margin-right: 12;
+	color: ${({theme}) => theme.colors.BlackInactive};
+	font-size: ${fonts.regular};
 	font-family: ${fonts.circularStdBold};
 `;
 
-
 export const InputDescription = styled.TextInput`
-  color: ${({ theme }) => theme.colors.BlackInactive};
-  font-size: ${fonts.regular};
-  font-family: ${fonts.circularStdBook};
+	color: ${({theme}) => theme.colors.BlackInactive};
+	font-size: ${fonts.regular};
+	font-family: ${fonts.circularStdBook};
 	margin-left: 25px;
 	width: 350px;
 	height: 50px;
