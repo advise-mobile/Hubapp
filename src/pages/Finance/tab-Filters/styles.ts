55d27e0a7fb7bce8a0a02949396283176e39747b
Julio <juliohn@gmail.com>
2025-03-12 21:33:02 -0300

    Ajuste do tamanho dos filtros na tela inicial do financeiro
    Os filtros, Este mes, hoje, esta semana e todos os periodos foram ajustado conforme o figma

--- src/pages/Finance/tab-Filters/styles.ts
+++ src/pages/Finance/tab-Filters/styles.ts
@@ -1,21 +1,13 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-	align-items: center;
-	min-width: 160px;
-	margin-bottom: 2px;
-	height: 32px;
-	flex-direction: row;
-`;
+export const Container = styled.View``;
 
 export const ContainerItems = styled.TouchableOpacity`
-	margin-left: 10px;
-	margin-right: 10px;
-	/*  */
+	padding: 4px 16px;
+	margin-left: 12px;
 	height: 32px;
 	align-items: center;
 	justify-content: center;
-	flex: 1;
 	border-radius: 4px;
 	border: ${({theme}) => theme.colors.BlackInactive};
 `;
