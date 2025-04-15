6be0010ab9ebeab33303c39d2400ea53941612e2
Julio <juliohn@gmail.com>
2025-04-14 21:47:31 -0300

    Ajuste do tamanho do loading
    quando confirm modal é chamado, ajustei o tamanho do loading ao confirmar a ação

--- src/components/ConfirmModal/index.tsx
+++ src/components/ConfirmModal/index.tsx
@@ -1,52 +1,41 @@
-import React, { forwardRef } from 'react';
+import React, {forwardRef} from 'react';
 
 import Modal from '@components/Modal';
 
 import Spinner from '@components/Spinner';
 
-import { ConfirmModalProps } from './types';
+import {ConfirmModalProps} from './types';
 
-import {
-  Footer,
-  Cancel,
-  CancelText,
-  Submit,
-  SubmitText,
-  Content,
-  Message
-} from './styles';
+import {Footer, Cancel, CancelText, Submit, SubmitText, Content, Message} from './styles';
 
 // Add Hook UseTheme para pegar o tema global addicionado
-import { useTheme } from 'styled-components';
+import {useTheme} from 'styled-components';
 
-export default ConfirmModal = forwardRef((props:ConfirmModalProps, ref) => {
-  
-     	// Variavel para usar o hook
+export default ConfirmModal = forwardRef((props: ConfirmModalProps, ref) => {
+	// Variavel para usar o hook
 	const colorUseTheme = useTheme();
-	const { colors } = colorUseTheme;
+	const {colors} = colorUseTheme;
 
+	const footer = () => (
+		<Footer>
+			<Cancel onPress={() => props.onCancel()}>
+				<CancelText>{props.cancelText}</CancelText>
+			</Cancel>
+			<Submit onPress={() => props.onSubmit()}>
+				{props.loading ? (
+					<Spinner transparent={true} color={colors.white} height="14" />
+				) : (
+					<SubmitText>{props.submitText}</SubmitText>
+				)}
+			</Submit>
+		</Footer>
+	);
 
-    const footer = () => (
-        <Footer>
-        <Cancel onPress={() => props.onCancel()}>
-            <CancelText>{props.cancelText}</CancelText>
-        </Cancel>
-        <Submit  onPress={() => props.onSubmit()}>
-            {
-              props.loading ? 
-              <Spinner transparent={true} color={colors.white} height='auto' /> :
-              <SubmitText>{props.submitText}</SubmitText>
-            }
-        </Submit>
-        </Footer>
-    );
-
-  return (
-    
-    <Modal ref={ref}  title={props.title}  footer={footer()}>
-      <Content>
-          <Message>{props.description}</Message>
-    </Content>
-    </Modal>
-  );
+	return (
+		<Modal ref={ref} title={props.title} footer={footer()}>
+			<Content>
+				<Message>{props.description}</Message>
+			</Content>
+		</Modal>
+	);
 });
