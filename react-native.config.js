5c8a0339cf90f06f8975d72eb38637e061120427
Julio <juliohn@gmail.com>
2025-08-02 15:13:53 -0300

    rodando update 2

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-	assets: ['./src/assets/fonts/'],
 	dependencies: {
-		'react-native-safe-area-context': {},
+		'react-native-safe-area-context': {
+			platforms: {
+				android: {
+					sourceDir: '../node_modules/react-native-safe-area-context/android',
+					packageImportPath: 'import io.th0th.reactnativesafeareacontext.SafeAreaContextPackage;',
+				},
+				ios: {},
+			},
+		},
+		microdiff: {
+			platforms: {
+				android: null, // disable Android platform, other platforms will still autolink
+				ios: null, // disable iOS platform, other platforms will still autolink
+			},
+		},
 	},
 };
