6bf0f75dc517622558429bfdc5e07f47578c306c
Julio <juliohn@gmail.com>
2025-08-26 19:44:21 -0300

    Priorizando alteracao de foto do redux
    Priorizando alteracao de foto do redux para o bottomTab

--- src/components/UserIcon/index.js
+++ src/components/UserIcon/index.js
@@ -1,50 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import jwtDecode from 'jwt-decode';
 
-import { Icon, IconText, IconImage } from './styles';
+import {Icon, IconText, IconImage} from './styles';
 
-import { AVATAR, TOKEN } from 'helpers/StorageKeys';
+import {AVATAR, TOKEN} from 'helpers/StorageKeys';
 
 const UserIcon = props => {
-  const [initials, setInitials] = useState();
-  const [avatar, setAvatar] = useState();
-  const picture = useSelector(state => state.user.picture);
+	const [initials, setInitials] = useState();
+	const [avatar, setAvatar] = useState();
+	const picture = useSelector(state => state.user.picture);
 
-  useEffect(() => {
-    AsyncStorage.getItem(AVATAR).then(image => setAvatar(image))
-      .finally(() => {
-        if (!avatar) {
-          if (props.name) {
-            let splittedName = props.name.split(" ");
-            let initials = splittedName.length == 1 ? splittedName[0][0] : `${splittedName[0][0]}${splittedName[splittedName.length - 1][0]}`;
+	useEffect(() => {
+		// Prioriza o picture do Redux se existir
+		if (picture) {
+			setAvatar(picture);
+		} else {
+			// Fallback para AsyncStorage
+			AsyncStorage.getItem(AVATAR).then(image => {
+				if (image) {
+					setAvatar(image);
+				}
+			});
+		}
 
-            setInitials(initials);
-          } else {
-            AsyncStorage.getItem(TOKEN).then(token => {
-              const userInfos = jwtDecode(token);
+		// Gera iniciais se não houver avatar
+		if (!picture && !avatar) {
+			if (props.name) {
+				let splittedName = props.name.split(' ');
+				let initials =
+					splittedName.length == 1
+						? splittedName[0][0]
+						: `${splittedName[0][0]}${splittedName[splittedName.length - 1][0]}`;
+				setInitials(initials);
+			} else {
+				AsyncStorage.getItem(TOKEN).then(token => {
+					const userInfos = jwtDecode(token);
+					let splittedName = userInfos.nome.split(' ');
+					let initials =
+						splittedName.length == 1
+							? splittedName[0][0]
+							: `${splittedName[0][0]}${splittedName[splittedName.length - 1][0]}`;
+					setInitials(initials);
+				});
+			}
+		}
+	}, [props, picture]);
 
-              let splittedName = userInfos.nome.split(" ");
-              let initials = splittedName.length == 1 ? splittedName[0][0] : `${splittedName[0][0]}${splittedName[splittedName.length - 1][0]}`;
-
-              setInitials(initials);
-            });
-          }
-        }
-      });
-  }, [props, picture]);
-
-  return (
-    <Icon {...props} active={props.avatar ? true : false}>
-      {avatar ? (
-        <IconImage source={{ uri: `data:image/png;charset=utf-8;base64,${avatar}` }} size={props.size || false} />
-      ) : (
-        <IconText>{initials}</IconText>
-      )}
-    </Icon>
-  )
+	return (
+		<Icon {...props} active={props.avatar ? true : false}>
+			{avatar ? (
+				<IconImage
+					source={{uri: `data:image/png;charset=utf-8;base64,${avatar}`}}
+					size={props.size || false}
+				/>
+			) : (
+				<IconText>{initials}</IconText>
+			)}
+		</Icon>
+	);
 };
 
 export default UserIcon;
