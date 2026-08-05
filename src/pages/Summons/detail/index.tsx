8ec6bf127cbc81cd8ba7964a262d7048c1fbb219
juliohn <juliohn@gmail.com>
2026-08-04 21:53:08 -0300

    #modulo-intimacoes
    Correção item 12 do QA, módulo Intimações: fecha e limpa o modal de enviar por email após sucesso no detalhe.

--- src/pages/Summons/detail/index.tsx
+++ src/pages/Summons/detail/index.tsx
@@ -298,6 +298,7 @@ export default function SummonsDetail() {
 				<SummonsSendEmailModal
 					visible={sendEmailVisible}
 					onClose={closeSendEmailModal}
+					onSuccess={closeSendEmailModal}
 					idMovProcessoCliente={idMovProcessoCliente}
 				/>
 				<SummonsDeleteConfirmModal
